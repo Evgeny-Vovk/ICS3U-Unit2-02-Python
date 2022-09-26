d62364d61e484f0fc6867f23d5ac9fd24694c592
Evgeny Vovk <Evgeny.vovk@mths.ca>
2022-09-26 15:25:08 +0000

    done with unit2-02 python

---
+++ ICS3U-Unit2-02.py
@@ -0,0 +1,29 @@
+# Copyright (c) 2022 Evgeny Vovk All rights reserved
+#
+# Created by: Evgeny Vovk
+# Created on: September 2022
+# ICS3U-Unit2-02.py File, learning input process and output in python
+
+import math
+from turtle import width
+
+
+def main():
+
+    # input
+    length_of_rectangle = int(input("Type in the length of your rectangle(mm): "))
+    width_of_rectangle = int(input("Type in the width of your rectangle(mm): "))
+
+    # process
+    area_of_rectangle = length_of_rectangle * width_of_rectangle
+    perimeter_of_rectangle = 2 * (length_of_rectangle + width_of_rectangle)
+
+    # output
+    print("\nThe area of the rectangle is {} mm².".format(area_of_rectangle))
+    print("The perimeter of the rectangle is {} mm.".format(perimeter_of_rectangle))
+
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
